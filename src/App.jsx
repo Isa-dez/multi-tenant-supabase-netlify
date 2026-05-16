5dd5a07a4530d21ed1b598c12ecefb594ae29dbb
Isa-dez <9s24rytwhp@privaterelay.appleid.com>
2026-05-16 15:41:31 -0700

    Add Login and Signup routes to App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import OrgHome from './routes/[orgSlug]/index'
 import OrgProjectsPage from './routes/[orgSlug]/projects'
+import Login from './routes/Login'
+import Signup from './routes/Signup'
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+
         <Route path="/:orgSlug" element={<OrgHome />} />
         <Route path="/:orgSlug/projects" element={<OrgProjectsPage />} />
       </Routes>
